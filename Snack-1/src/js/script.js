b45e855086c7450c22eb47c3c9c51bcb65071444
Steopa2001 <moraru495@gmail.com>
2025-07-14 16:17:47 +0200

    fix (snack-1): change constant name

--- Snack-1/src/js/script.js
+++ Snack-1/src/js/script.js
@@ -1,7 +1,15 @@
 
 // creo array di oggetti
-let persone = [
+let bici = [
     {nome: 'Canyon', peso: 6.5},
     {nome: 'Specialized', peso: 5.9},
     {nome: 'Trek', peso: 6.8}
 ];
+
+
+
+// const lighterWeight =
+
+// for (let i = 0; i < persone.length; i++) {
+
+// }
