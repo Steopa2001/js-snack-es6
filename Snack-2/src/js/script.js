ba34a9b8ead1750d76db306f09f5b722c34ee652
Steopa2001 <moraru495@gmail.com>
2025-07-14 16:45:22 +0200

    feat (snack-2): create arrays of objects

---
+++ Snack-2/src/js/script.js
@@ -0,0 +1,9 @@
+//creo un array di oggetti di squadre di calcio
+const squadreCalcio = [
+    {nome: 'Bologna', puntiFatti: 0, falliSubiti: 0},
+    {nome: 'Inter', puntiFatti: 0, falliSubiti: 0},
+    {nome: 'Juventus', puntiFatti: 0, falliSubiti: 0},
+    {nome: 'Milan', puntiFatti: 0, falliSubiti: 0},
+];
+
+
