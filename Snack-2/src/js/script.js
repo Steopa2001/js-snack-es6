bd56bc5a07b9dc677abb4a070fe8e08f7bb2a31a
Steopa2001 <moraru495@gmail.com>
2025-07-14 17:08:56 +0200

    feat (snack-2): create new array of Name and make them Immediately

--- Snack-2/src/js/script.js
+++ Snack-2/src/js/script.js
@@ -12,3 +12,28 @@ for (let i = 0; i < squadreCalcio.length; i++) {
     squadreCalcio[i].falliSubiti = Math.floor(Math.random() * 81);     // da 0 a 80
 }
 
+
+// // creo un nuovo array con solo nome e falliSubiti usando un arrow function
+// const nuovoArray = squadreCalcio.map(squadra => ({
+//   nome: squadra.nome,
+//   falliSubiti: squadra.falliSubiti
+// }));
+
+// array vuoto dove mettere i nuovi oggetti
+const nuovoArray = []; 
+
+for (let i = 0; i < squadreCalcio.length; i++) {
+  const squadra = squadreCalcio[i];  
+
+  // creo un nuovo oggetto con solo nome e falliSubiti
+  const obj = {
+    nome: squadra.nome,
+    falliSubiti: squadra.falliSubiti
+  };
+
+  // aggiungo l'oggetto al nuovo array
+  nuovoArray.push(obj);
+}
+
+console.log(nuovoArray);
+
